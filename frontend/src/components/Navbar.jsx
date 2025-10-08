2c6b3073ca5fa15301690ead43992d1718b96918
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2025-10-08 22:59:30 +0800

    changed it into side bar with animation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { BsChevronBarLeft, BsChevronBarRight } from "react-icons/bs";
+import { MdMoreVert } from "react-icons/md";
 
-const Navbar = () => {
+const SideBarContext = createContext();
+const Navbar = ({ children }) => {
   const THEMES = [
     'light',
     'forest',
@@ -17,6 +20,7 @@ const Navbar = () => {
   ];
 
   const [theme, setTheme] = useState('light');
+  const [expanded, setExpanded] = useState(false);
   const handleChangeTheme = (e) => {
     setTheme(e.target.value);
   };
@@ -24,58 +28,80 @@ const Navbar = () => {
     document.documentElement.setAttribute('data-theme', theme);
   }, [theme]);
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li><a>Item 1</a></li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
-          </ul>
+    <aside className="h-screen">
+      <nav className={`h-full flex flex-col bg-white border-r border-gray-200 shadow-sm transition-all duration-300 ${expanded ? 'w-64' : 'w-18'
+        }`}>
+
+        {/*  */}
+        <div className="p-4 pb-2 flex justify-between items-center mb-10">
+          <img src='/images/nav-logo.png' alt=""
+            className={` overflow-hidden transition-all ${expanded ? 'size-8' : 'size-0'}`} />
+          <button onClick={() => setExpanded((prev) => !prev)}>
+            {
+              expanded ? <BsChevronBarRight /> : <BsChevronBarLeft />
+            }
+          </button>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li><a>Item 1</a></li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </details>
-          </li>
-          <li><a>Item 3</a></li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <select className="px-2 py-1 flex items-center justify-between text-sm" onChange={handleChangeTheme}
-        >
-          <option value="" disabled>Select a theme</option>
-          {
-            THEMES.map((theme, index) => (
-              <option key={index} value={theme} >
-                {theme.charAt(0).toLocaleUpperCase() + theme.slice(1)}
-              </option>
-            ))
-          }
-        </select>
-      </div>
-    </div>
+
+        {/*  */}
+        <SideBarContext.Provider value={{ expanded }}>
+          <ul className=" flex-1 px-3 ">
+            {children}
+          </ul>
+        </SideBarContext.Provider>
+        {/* bottom */}
+        <div className="border-t flex p-3 items-center border-gray-200 ">
+          <img src={'/images/nav-logo.png'} alt="" className="rounded-md size-8 min-w-[2rem]" />
+          <div className={`flex justify-between items-center overflow-hidden transition-all ${expanded ? "w-52 ml-3" : 'w-0'}`}>
+            <div className="leading-4">
+              <h4 className="font-semibold">John Doe</h4>
+              <span className="text-xs text-gray-600">Johndoe@gmail.com</span>
+            </div>
+          </div>
+          <MdMoreVert
+            size={20}
+            className={`transition-all duration-300 ${expanded ? 'ml-auto' : 'opacity-0 w-0'
+              }`}
+          />
+        </div>
+      </nav>
+
+    </aside>
   );
 };
-
 export default Navbar;
+export function SideBarItem({ icon, text, active, alert }) {
+  const { expanded } = useContext(SideBarContext);
+  return (
+    <li className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group ${active ? "bg-gradient-to-r from-indigo-200 to-indigo-100 text-indigo-800" : 'hover:bg-indigo-50 text-gray-600 '
+      }`}>
+      {icon}
+      <span className={`overflow-hidden transition-all ${expanded ? "w-52 ml-3" : 'w-0'}`}>{text}</span>
+      {
+        alert && <div className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${expanded ? ''
+          : 'top-2'}`}></div>
+      }
+      {!expanded && (
+        <div className="absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0 z-50">
+          {
+            text
+          }
+        </div>
+      )}
+    </li>
+  );
+
+}
+//  <div className="">
+//         <select className="px-2 py-1 flex items-center justify-between text-sm" onChange={handleChangeTheme}
+//         >
+//           <option value="" disabled>Select a theme</option>
+//           {
+//             THEMES.map((theme, index) => (
+//               <option key={index} value={theme} >
+//                 {theme.charAt(0).toLocaleUpperCase() + theme.slice(1)}
+//               </option>
+//             ))
+//           }
+//         </select>
+//       </div>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,41 @@
 import React from "react";
-import Navbar from "../components/Navbar.jsx";
-
+import Navbar, { SideBarItem } from "../components/Navbar.jsx";
+import {
+  LifeBuoy,
+  Receipt,
+  Boxes,
+  Package,
+  UserCircle,
+  BarChart3,
+  LayoutDashboard,
+  Settings,
+  User
+} from 'lucide-react';
 const Dashboard = () => {
-  return <div>
-    <Navbar />
+  return <div className="h-screen w-full flex">
+    <Navbar>
+      <SideBarItem icon={<LayoutDashboard size={20} />} text={'Dashboard'}
+        alert />
+      <SideBarItem icon={<BarChart3 size={20} />} text={'Statistics'} active
+      />
+      <SideBarItem icon={<UserCircle size={20} />} text={'Users'} active
+      />
+      <SideBarItem icon={<Boxes size={20} />} text={'Inventory'}
+      />
+      <SideBarItem icon={<Package size={20} />} text={'Orders'}
+        alert />
+      <SideBarItem icon={<Receipt size={20} />} text={'Billings'}
+      />
+      <hr className="my-3 border-gray-100" />
+      <SideBarItem icon={<Settings size={20} />} text={'Settings'}
+      />
+      <SideBarItem icon={<LifeBuoy size={20} />} text={'Help'}
+      />
+    </Navbar>
+    {/* Main content here */}
+    <main>
+      <h1>Main content here</h1>
+    </main>
   </div>;
 };
 
